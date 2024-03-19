3211d7473a9b493070345716ffaab54f2e491f1c
thomasjodt <jodt5200@gmail.com>
2024-03-19 18:57:03 -0500

    feat(models): Create a model for the error response

---
+++ src/main/java/org/jodt5/invoices/models/utilities/RequestError.java
@@ -0,0 +1,34 @@
+package org.jodt5.invoices.models.utilities;
+
+public class RequestError {
+    private int status;
+    private String message;
+    private String description;
+
+    public RequestError() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
